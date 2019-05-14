57e5318ec63ff01ada28a11ea67011a901d61d40
DonMahallem <donmahallem@users.noreply.github.com>
2019-05-14 17:03:57 +0200

    JJ

--- download_build_app.sh
+++ download_build_app.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 git clone https://github.com/donmahallem/TrapezeClientNg.git
 cd TrapezeClientNg
+cp ./../build.environment.ts ./src/environments/environment.prod.ts
+cp ./../build.environment.ts ./src/environments/environment.ts
+cp ./../build.environment.ts ./src/environments/environment.dev.ts
 npm install
 ng build --aot --prod --base-href=/TrapezeClientNgDemo/
 cd ..
