6d9e5f994ac565006c3031568bfedce27ea885a1
DonMahallem <donmahallem@users.noreply.github.com>
2019-05-15 13:35:15 +0200

    copy new manifest to subdir

--- download_build_app.sh
+++ download_build_app.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 FOLDER="./TrapezeClientNg"
 echo "switch dir"
-ls
 cd "$FOLDER"
 echo "in dir"
-ls
 #copy envs
 cp ./src/environments/environment.example.pwa.ts ./src/environments/environment.prod.ts
 sed -i 's/apiEndpoint\x3A\x20\x27\x2F\x27/apiEndpoint\x3A\x20\x27\x2FTrapezeClientNgDemo\x2F\x27/g' ./src/environments/environment.prod.ts
 cp ./src/environments/environment.prod.ts ./src/environments/environment.ts
 cp ./src/environments/environment.prod.ts ./src/environments/environment.dev.ts
+cp ./../manifest.json ./src/manifest.json -f
 echo "Start Install"
 npm install --no-save
 echo "Start Build"
