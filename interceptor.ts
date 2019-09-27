bef56965667e923ffad79c9c895cf561ae3bb256
donmahallem <donmahallem@users.noreply.github.com>
2019-09-27 13:55:15 +0200

    JJ

--- interceptor.ts
+++ interceptor.ts
@@ -5,11 +5,10 @@ import {
     HttpEvent,
     HttpInterceptor
 } from '@angular/common/http';
-import { AuthService } from './auth/auth.service';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-    constructor(public auth: AuthService) { }
+    constructor() { }
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
         request = request.clone({
