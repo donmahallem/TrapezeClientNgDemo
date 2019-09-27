16b0be9afc491ebac2a1e0ac5e9e0f7de79637dc
donmahallem <donmahallem@users.noreply.github.com>
2019-09-27 12:54:52 +0200

    b

--- download_build_app.sh
+++ download_build_app.sh
@@ -6,10 +6,15 @@ cd TrapezeClientNg
 #copy envs
 cp ./src/environments/environment.example.pwa.ts ./src/environments/environment.prod.ts
 sed -i 's/apiEndpoint\x3A\x20\x27\x2F\x27/apiEndpoint\x3A\x20\x27\x2FTrapezeClientNgDemo\x2F\x27/g' ./src/environments/environment.prod.ts
+sed -i 's/providers\:\x20\[/providers\:\x20\[\{provide\:\x20HTTP_INTERCEPTORS\,useClass\:\x20TokenInterceptor\,multi\:\x20true\}\,/g' ./src/app/app.module.ts
+sed -i -e '1s;^;import\x20{TokenInterceptor}\x20from\x20\"./interceptor.ts\"\;\n;' ./src/app/app.module.ts
+sed -i -e '1s;^;import\x20{HTTP_INTERCEPTORS}\x20from\x20\"\@angular\/common\/http\"\;\n;' ./src/app/app.module.ts
 cp ./src/environments/environment.prod.ts ./src/environments/environment.ts
 cp ./src/environments/environment.prod.ts ./src/environments/environment.dev.ts
+cp ./../interceptor.ts ./src/app/interceptor.ts
+
 echo "Start Install"
 npm install
 echo "Start Build"
-ng build --aot --prod --base-href=/TrapezeClientNgDemo/
+#ng build --aot --prod --base-href=/TrapezeClientNgDemo/
 cd ..
