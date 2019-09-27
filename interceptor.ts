7d9660c4e0bf2e3abccacf2c5c5b7fa13cbbbb9b
donmahallem <donmahallem@users.noreply.github.com>
2019-09-27 15:11:14 +0200

    bump

--- interceptor.ts
+++ interceptor.ts
@@ -5,17 +5,27 @@ import {
     HttpEvent,
     HttpInterceptor
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, defer } from 'rxjs';
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
     constructor() { }
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-        request = request.clone({
-            setHeaders: {
-                Authorization: 'Booomstick'
-            }
-        });
-        return next.handle(request);
+        return defer(this.digestMessage(navigator.userAgent))
+            .flatMap((val) => {
+                request = request.clone({
+                    setHeaders: {
+                        Authorization: val
+                    }
+                });
+                return next.handle(request);
+            })
     }
+    async digestMessage(message) {
+        const msgUint8 = new TextEncoder().encode(message);                           // encode as (utf-8) Uint8Array
+        const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);           // hash the message
+        const hashArray = Array.from(new Uint8Array(hashBuffer));                     // convert buffer to byte array
+        const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
+        return hashHex;
+    }
+
 }
