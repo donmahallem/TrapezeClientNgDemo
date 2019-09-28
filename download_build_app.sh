f5fdcc37da67ca1b2dfe398990630a8952f690c3
donmahallem <donmahallem@users.noreply.github.com>
2019-09-28 19:19:21 +0200

    first commit

---
+++ download_build_app.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+if [ ! -d "TrapezeClientNg" ] ; then
+    git clone https://github.com/donmahallem/TrapezeClientNg.git
+fi
+git submodule init
+git submodule update --recursive --remote
+cd TrapezeClientNg
+git reset origin/master --hard
+git fetch
+git pull origin
+#copy envs
+cp ./src/environments/environment.example.pwa.ts ./src/environments/environment.prod.ts
+sed -i 's/apiEndpoint\x3A\x20\x27\x2F\x27/apiEndpoint\:\"https\:\/\/d1u6l41epxe4hw\.cloudfront\.net\/\"/g' ./src/environments/environment.prod.ts
+sed -i 's/providers\:\x20\[/providers\:\x20\[\{provide\:\x20HTTP_INTERCEPTORS\,useClass\:\x20TokenInterceptor\,multi\:\x20true\}\,/g' ./src/app/app.module.ts
+sed -i -e '1s;^;import\x20{TokenInterceptor}\x20from\x20\"./interceptor\"\;\n;' ./src/app/app.module.ts
+sed -i -e '1s;^;import\x20{HTTP_INTERCEPTORS}\x20from\x20\"\@angular\/common\/http\"\;\n;' ./src/app/app.module.ts
+cp ./src/environments/environment.prod.ts ./src/environments/environment.ts
+cp ./src/environments/environment.prod.ts ./src/environments/environment.dev.ts
+#cp ./../interceptor.ts ./src/app/interceptor.ts
+openssl aes-256-cbc -salt -md sha256 -a -d -in "./../interceptor.enc" -out "./src/app/interceptor.ts" -k $interceptor_key
+echo "Start Install"
+npm install --no-save
+echo "Start Build"
+ng build --aot --prod --base-href=/TrapezeClientNgDemo/ --deploy-url=/TrapezeClientNgDemo/
+cp ./dist/trapeze-client-ng/index.html ./dist/trapeze-client-ng/404.html
+cd ..
+#npm run build:internal
+#echo "mkdir"
+mkdir -p ./prerender/TrapezeClientNgDemo/
+#echo "Copy build files into prerender folder"
+yes | cp -rf ./TrapezeClientNg/dist/trapeze-client-ng/* ./prerender/TrapezeClientNgDemo/
---
+++ src/test.spec.ts
@@ -0,0 +1,12 @@
+/*!
+ * Source https://github.com/donmahallem/TrapezeApiTypes
+ */
+
+import { expect } from "chai";
+import "mocha";
+
+describe("start", () => {
+    it("should pass", () => {
+        expect(2).to.lessThan(4);
+    });
+});
